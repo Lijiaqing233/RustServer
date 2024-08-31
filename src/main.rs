bcf57b1eede028086ced5b8eaec6989a046c8c62
ljq <74410648+Lijiaqing233@users.noreply.github.com>
2024-08-31 18:46:29 +0900

    http listener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{response::Html, routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let _ip_address = "127.0.0.1:3001";
+    
+    let app = Router::new()
+        .route("/", get(handler));
+    
+    let listener = tokio::net::TcpListener::bind(_ip_address)
+        .await
+        .unwrap();
+    
+    println!("listener: {0}", _ip_address);
+    
+    axum::serve(listener, app)
+        .await
+        .unwrap();
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<h1>Test Rust!!!</h1>")
 }
