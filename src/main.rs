5e76048f9fb47c2d0748e605ef3035c3295a477e
ljq <74410648+Lijiaqing233@users.noreply.github.com>
2024-09-01 20:12:47 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 use axum::{response::Html, routing::get, Router};
+use axum::extract::Path;
+use axum::extract::Query;
+use std::collections::HashMap;
 
 #[tokio::main]
 async fn main() {
     let _ip_address = "127.0.0.1:3001";
     
     let app = Router::new()
-        .route("/", get(handler));
+        .route("/", get(handler))
+        .route("/book/:id", get(path_extract))
+        .route("/book", get(query_extract));
     
     let listener = tokio::net::TcpListener::bind(_ip_address)
         .await
@@ -20,4 +25,16 @@ async fn main() {
 
 async fn handler() -> Html<&'static str> {
     Html("<h1>Test Rust!!!</h1>")
+}
+
+async fn path_extract(
+    Path(id): Path<u32>,
+) -> Html<String> {
+    Html(format!("Hello, {id}！"))
+}
+
+async fn query_extract(
+    Query(params): Query<HashMap<String, String>>
+) -> Html<String> {
+    Html(format!("{params:#?}"))
 }
